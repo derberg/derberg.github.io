473da0d4468c34596aa046ff7c62339b32c419af
derberg <lpgornicki@gmail.com>
2021-12-29 16:59:33 +0100

    remove not needed analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
@@ -19,7 +18,6 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
