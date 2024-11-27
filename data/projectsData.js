c8fb70138b8613bc48d04a4ef2dba7aa33e742b1
Lukasz Gornicki <lpgornicki@gmail.com>
2024-11-27 17:09:51 +0100

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,13 +1,13 @@
 const projectsData = [
   {
     title: 'AsyncAPI Specification',
-    description: `The AsyncAPI specification allows you to create machine-readable definitions of your asynchronous APIs. I'm one of maintainers of the specification.`,
+    description: `The AsyncAPI specification allows you to create machine-readable definitions of your asynchronous APIs. I'm one of the maintainers of the specification that can be easily validated by visiting [CODEOWNERS file](https://github.com/asyncapi/spec/blob/master/CODEOWNERS).`,
     imgSrc: '/static/images/asyncapi.png',
     href: 'https://github.com/asyncapi/spec',
   },
   {
     title: 'AsyncAPI Generator',
-    description: `Generator is a tool that you can use to generate whatever you want (code, docs) basing on the AsyncAPI document as an input. I'm one of maintainers.`,
+    description: `Generator is a tool that you can use to generate whatever you want (code, docs) basing on the AsyncAPI document as an input. I'm one of the maintainers of the generator that can be easily validated by visiting [CODEOWNERS file](https://github.com/asyncapi/generator/blob/master/CODEOWNERS).`,
     imgSrc: '/static/images/generator.png',
     href: 'https://github.com/asyncapi/generator',
   },
